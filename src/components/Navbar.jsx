8da62c49cc1eeac211f5385249ce8e7fae2d3214
root <root@BaoAu>
2025-02-10 15:07:42 +0700

    navBar auto mx need to be fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { styles } from '../styles';
+import { styles } from '../styles.js';
 import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 import { li, linearGradient } from 'framer-motion/client';
@@ -9,11 +9,11 @@ import { li, linearGradient } from 'framer-motion/client';
 const Navbar = () => {
 
   const [active, setActive] = useState(" ");
-
+  const [toggle, setToggle] = useState(false);
   return (
 
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#050816]`}
+      className={`sm:px-16 px-2 w-full flex items-center py-5 fixed top-0 z-20 bg-[#050816]`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
@@ -25,8 +25,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>BAO
-            <span className='sm:block hidden'>| THAT VIET CODER</span>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>BAO &nbsp;
+            <span className='sm:block hidden'>| &nbsp; THAT VIET CODER</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -34,8 +34,8 @@ const Navbar = () => {
             <li
               key={link.id}
               className={`${active === link.title
-                  ? "text-white"
-                  : "text-[#aaa6c3]"
+                ? "text-white"
+                : "text-[#aaa6c3]"
                 } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
@@ -43,8 +43,31 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <div>
-          
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu} alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 justify-center black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none py-4 flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                    ? "text-white"
+                    : "text-[#aaa6c3]"
+                    } font-poppins py-4 font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
