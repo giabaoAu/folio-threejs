4abe7dfd19659ffc7fea019510418979674e1ca2
root <root@BaoAu>
2025-02-10 19:55:16 +0700

    computer section to be futher more implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px]  max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <section className={`relative h-screen `}>
+      <div className={`px-6 sm:px-16 absolute inset-0 top-[120px] mr-0 flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -20,9 +20,9 @@ const Hero = () => {
             interfaces and web applications
           </p>
         </div>
-
-        <ComputersCanvas />
       </div>
+
+      <ComputersCanvas />
     </section >
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+
+  const computer = useGLTF('./desktop.pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+        groundColor="black" />
+      <pintlight intensity={1} />
+      <primitive
+        object={computer.scene}
+      />
+    </mesh>
+  )
+} 
+const ComputersCanvas = () => {
+  return (
+    <Canvas>
+
+    </Canvas>
   )
 }
-
 export default Computers
