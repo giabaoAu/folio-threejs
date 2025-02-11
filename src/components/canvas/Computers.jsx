4aa66b229f54d71efc10d68fcb4abbdfc66dfbed
root <root@BaoAu>
2025-02-11 15:07:16 +0700

    add try catch for model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,13 @@ import CanvasLoader from "../Loader";
 
 const Computers = () => {
 
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  let computer;
+  try {
+    computer = useGLTF("/desktop_pc/scene.gltf");
+  } catch (error) {
+    console.error("Error loading model:", error);
+    return null; // Prevents crashing if model fails
+  }
 
   console.log(computer);
 
@@ -16,9 +22,22 @@ const Computers = () => {
         intensity={0.15}
         groundColor="black"
       />
+
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
